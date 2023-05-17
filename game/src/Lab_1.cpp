407d3613a72928871953661c99505c60507629e7
Duc Tran <101432125@georgebrown.ca>
2023-05-17 09:55:05 -0400

    Updated code

--- game/src/Lab_1.cpp
+++ game/src/Lab_1.cpp
@@ -1,37 +1,24 @@
 #include "raylib.h"
 
+// InitWindow(800, 600, "Raylib Function Examples") : Initializes a window with the specified width, height, and title.This function is necessary for creating the game window.
 
-// This function initializes a window with the specified width and height. It's useful because it sets up the basic window infrastructure required to render graphics.
-// InitWindow(640, 480, "My Window");
+// SetTargetFPS(60) : Sets the desired frame rate per second.It ensures that the game loop runs at a consistent frame rate, providing smooth animations and gameplay.
 
-// This function sets the target frame rate for the application. It's useful because it can help to smooth out animations and make the application feel more responsive.
-// SetTargetFPS(60);
+// WindowShouldClose() : Checks if the user has requested to close the window(e.g., clicked the close button).It's used in the loop condition to exit the program when the window is closed.
 
-// This function sets up the rendering environment for the current frame. It's useful because it allows you to draw multiple graphics primitives within the same frame.
-// BeginDrawing();
+// GetMousePosition() : Retrieves the current position of the mouse cursor on the screen.We use this function to control the position of the second circle.
 
-// This function draws a string of text on the screen at the specified coordinates. It's useful for displaying information or user interfaces.
-// DrawText("Hello, world!", 100, 100, 20, BLACK);
+// CheckCollisionCircles(circle1Position, circle1Radius, circle2Position, circle2Radius) : Checks if two circles overlap.In our case, we use it to determine if the two circles intersect.
 
-// This function draws a rectangle on the screen at the specified coordinates and with the specified dimensions. It's useful for drawing shapes and backgrounds.
-// DrawRectangle(200, 200, 50, 50, BLUE);
+// ClearBackground(RAYWHITE): Clears the screen with the specified color.It's called at the beginning of each frame to reset the screen before drawing.
 
-// This function loads an image from file into a texture. It's useful for creating custom graphics assets.
-// Texture myTexture = LoadTexture("my_texture.png");
+// BeginDrawing() : Marks the beginning of the rendering process.It prepares the screen for drawing objects and shapes.
 
-// This function releases the memory used by a texture. It's useful for managing resources and avoiding memory leaks.
-// UnloadTexture(myTexture);
+// EndDrawing() : Marks the end of the rendering process.It finalizes the rendering and presents the completed frame to the screen.
 
-// This function checks whether a key is currently pressed. It's useful for implementing keyboard-based controls.
-// if (IsKeyDown(KEY_SPACE)) {
-    // do something
-// }
+// DrawCircleV(circle1Position, circle1Radius, circle1Color) : Draws a circle with the specified position, radius, and color.We use this function to render the first circle.
 
-//  This function sets the position of the mouse cursor. It's useful for implementing custom mouse-based controls.
-// SetMousePosition(100, 100);
-
-// This function returns a random integer value between two specified values. It's useful for implementing random events or generating random game objects.
-// int myRandomValue = GetRandomValue(1, 10);
+// DrawCircleV(circle2Position, circle2Radius, circle2Color) : Draws the second circle with the specified position, radius, and color.The position of this circle is controlled by the mouse cursor.
 
 
             int main(void)
