5ad49b97ac9f8504bd5ff611a58f4011ab54804e
Duc Tran <101432125@georgebrown.ca>
2023-06-14 21:17:35 -0400

    Lab 2 completed

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,20 +1,91 @@
-#include "rlImGui.h"
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#include <raylib.h>
+#include <vector>
 
-int main(void)
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
+class Rigidbody {
+public:
+    Vector2 position;
+    Vector2 velocity;
+};
+
+class Agent {
+public:
+    Rigidbody rigidbody;
+    Texture2D sprite;
+    float maxSpeed;
+    float maxAcceleration;
+};
+
+void UpdateRigidbody(Rigidbody& rigidbody, float deltaTime) {
+    rigidbody.position += rigidbody.velocity * deltaTime;
+}
+
+Vector2 Seek(Vector2 agentPosition, Vector2 agentVelocity, Vector2 targetPosition, float maxAcceleration) {
+    Vector2 desiredVelocity = Vector2Normalize(targetPosition - agentPosition) * Vector2Length(agentVelocity);
+    Vector2 steering = desiredVelocity - agentVelocity;
+    return Vector2Clamp(steering, -maxAcceleration, maxAcceleration);
+}
+
+Vector2 Flee(Vector2 agentPosition, Vector2 agentVelocity, Vector2 targetPosition, float maxAcceleration) {
+    Vector2 desiredVelocity = Vector2Normalize(agentPosition - targetPosition) * Vector2Length(agentVelocity);
+    Vector2 steering = desiredVelocity - agentVelocity;
+    return Vector2Clamp(steering, -maxAcceleration, maxAcceleration);
+}
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Kinematics Simulation");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
+    std::vector<Agent> agents;
+    Agent agent1;
+    agent1.rigidbody.position = { 100, 200 };
+    agent1.rigidbody.velocity = { 100, 0 };
+    agent1.maxSpeed = 200.0f;
+    agent1.maxAcceleration = 100.0f;
+    agent1.sprite = LoadTexture("agent1.png");
+    agents.push_back(agent1);
+
+    Agent agent2;
+    agent2.rigidbody.position = { 500, 200 };
+    agent2.rigidbody.velocity = { -100, 0 };
+    agent2.maxSpeed = 150.0f;
+    agent2.maxAcceleration = 80.0f;
+    agent2.sprite = LoadTexture("agent2.png");
+    agents.push_back(agent2);
+
+    while (!WindowShouldClose()) {
+        // Update
+        float deltaTime = GetFrameTime();
+
+        for (auto& agent : agents) {
+            Vector2 targetPosition = GetMousePosition();
+
+            if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+                Vector2 acceleration = Seek(agent.rigidbody.position, agent.rigidbody.velocity, targetPosition, agent.maxAcceleration);
+                agent.rigidbody.velocity += acceleration * deltaTime;
+            }
+
+            UpdateRigidbody(agent.rigidbody, deltaTime);
+        }
+
+        // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+
+        for (const auto& agent : agents) {
+            DrawTexture(agent.sprite, agent.rigidbody.position.x, agent.rigidbody.position.y, WHITE);
+        }
+
         EndDrawing();
     }
 
+    for (const auto& agent : agents) {
+        UnloadTexture(agent.sprite);
+    }
+
     CloseWindow();
+
     return 0;
 }
